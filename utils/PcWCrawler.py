2b888ac8bf30e8a15fd31f66dd36be7646d7e3ef
EasyZ <707372295#qq.com>
2021-01-15 21:49:22 +0800

    first commit

---
+++ utils/PcWCrawler.py
@@ -0,0 +1,23 @@
+# coding : utf-8
+import sys
+sys.path.append('./utils/') # noqa
+from UrlCrawler import UrlCrawler # noqa
+from PDFDownloader import PDFDownloader # noqa
+from detailInfoCrawler import detailInfoCrawler
+from tqdm import tqdm # noqa
+
+class PcWCrawler:
+    def __init__(self):
+        self.urlCrawler = UrlCrawler()
+        self.pdfDownloader = PDFDownloader()
+        self.detailInfoCrawler = detailInfoCrawler()
+
+    def run(self):
+        self.urlCrawler.crawlAll()
+        self.detailInfoCrawler.crawlAllInfo()
+        self.pdfDownloader.downloadAll()
+        print("All crawlers finished.")
+
+if __name__ == '__main__':
+    pcWCrawler = PcWCrawler()
+    pcWCrawler.run()
